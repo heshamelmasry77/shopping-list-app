57e6def2da277a50bb5ae0ebaadb6e39e772d27a
Hesham <heshamelmasry1992@gmail.com>
2020-10-08 16:38:58 +0200

    make the form pretty by adding more styling

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,56 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+body {
+  background: #eff8ff;
+  height: 100vh;
+  width: 100vw;
+  font-family: system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+}
+
+
+/*GENERIC CLASSES FOR BTNS */
+.btn {
+  border: none;
+  border-radius: 3px;
+  margin: auto 0;
+  padding: .5rem .75rem;
+  flex-shrink: 0;
+  cursor: pointer;
+  font-size: .9rem;
+  letter-spacing: .5px;
+  transition: all .1s ease-in;
+}
+
+.btn[disabled] {
+  background: #8795a1;
+}
+
+.btn[disabled]:hover {
+  background: #606f7b;
+}
+
+.btn-primary {
+  background: #6cb2eb;
+  color: #fff;
+}
+
+.btn-primary:hover {
+  background: #3490dc;
+}
+
+.btn-cancel {
+  background: #ef5753;
+  color: #fff;
+}
+
+.btn-cancel:hover {
+  background: #e3342f;
+  color: #fff;
+}
 </style>
