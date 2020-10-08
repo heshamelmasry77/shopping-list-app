0c0d6ce76f700f850fe7a1179465bc3e096ad77c
Hesham <heshamelmasry1992@gmail.com>
2020-10-08 17:32:34 +0200

    add some icons etc

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Unicon from 'vue-unicons'
+import {uniTrophy} from 'vue-unicons/src/icons'
 
 Vue.config.productionTip = false
+Unicon.add([uniTrophy])
+Vue.use(Unicon)
 
 new Vue({
   render: h => h(App),
