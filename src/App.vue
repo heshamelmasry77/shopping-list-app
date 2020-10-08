6589559462da89d692d4464b6b1d6540c7684cb1
Hesham <heshamelmasry1992@gmail.com>
2020-10-08 14:03:04 +0200

    initial commit with some first steps add list

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ShoppingList header="Shopping List App"/>
+  </div>
+</template>
+
+<script>
+import ShoppingList from './components/ShoppingList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ShoppingList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="shoppingList">
+    <h1>{{ header }}</h1>
+    <ul v-for="(item, index) in items" :key="index">
+      <li>{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShoppingList',
+  props: {
+    header: String
+  },
+  data: function () {
+    return {
+      items: [
+        '10 eggs',
+        '2 apples',
+        '5 oranges'
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.shoppingList {
+  background-color: lightgray;
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 15px;
+}
+h1 {
+  text-transform: uppercase;
+  font-weight: bolder;
+  text-align: left;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  text-transform: capitalize;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: left;
+}
+</style>
