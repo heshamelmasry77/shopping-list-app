21c47f3186099b8a34f5817efefcb0b576f3edcf
Hesham <heshamelmasry1992@gmail.com>
2020-10-08 14:53:49 +0200

    add new item, some styles for the form, push to an array

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="shoppingList">
     <h1>{{ header }}</h1>
+    <label>
+      <input v-model="newItem" type="text" placeholder="Add new item" @keydown.enter="items.push(newItem)"/>
+      <button @click="items.push(newItem)">Save Item</button>
+    </label>
     <ul v-for="(item, index) in items" :key="index">
       <li>{{ item }}</li>
     </ul>
@@ -15,6 +19,7 @@ export default {
   },
   data: function () {
     return {
+      newItem: '',
       items: [
         '10 eggs',
         '2 apples',
@@ -32,11 +37,18 @@ export default {
   max-width: 1024px;
   margin: 0 auto;
   padding: 15px;
+  text-align: left;
 }
 h1 {
   text-transform: uppercase;
   font-weight: bolder;
   text-align: left;
+}
+input {
+  padding: 10px;
+  border: none;
+  display: inline-block;
+  width: 200px;
 }
 ul {
   list-style-type: none;
